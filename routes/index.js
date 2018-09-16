a85821a64c553c61ee5b310406f565d717b97598
Eben Palazzolo <olozzalap@gmail.com>
2018-09-16 18:31:24 -0400

    Setup for deploy to Heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,5 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send({ response: "active connection" }).status(200);
 });
-router.get("/abc", (req, res) => {
-  res.send({ response: "heyoooooo" }).status(200);
-});
+
 module.exports = router;
